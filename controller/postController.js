9e90e459e548f83b1158d38ca61411b49295a2d0
ridoy-islam-coder <rkrafikridoy5887@gmail.com>
2025-07-14 23:21:22 +0600

    dane

--- controller/postController.js
+++ controller/postController.js
@@ -1,23 +1,29 @@
-import prisma from "../DB/db.confige";
-import { Prisma } from "@prisma/client";
-export const createpost = async (req, res) => {
-    const { title, content } = req.body;
-    const { user_id } = req.params; 
-    if (!title || !content) {
-        return res.status(400).json({ message: "Title and content are required" }); 
-    }   
-    const post = await prisma.post.create({
-        data: {
-            title: title,
-            content: content,
-            user: {
-                connect: { id: user_id }
-            }
-        }
-    });
-    return res.status(201).json({ message: "Post created successfully", post });
-};
 
+import prisma from "../DB/db.confige.js";
+
+
+export const createpost = async (req, res) => {
+  const { user_id, title, content } = req.body;
+
+  if (!title || !content) {
+    return res.status(400).json({ message: "Title and content are required" });
+  }
+
+  try {
+    const post = await prisma.post.create({
+      data: {
+        title,
+        content,
+        user_id: user_id,
+      },
+    });
+
+    return res.status(201).json({ message: "Post created successfully", post });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "Something went wrong" });
+  }
+};
 export const getAllposts = async (req, res) => {
     const user= req.params.user_id;
     if (!user) {
--- router/userRotur.js
+++ router/userRotur.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 
 import { CreateUser, deleteuser, getAllUsers, updateuaser } from "../controller/userController.js";
+import { createpost, getAllposts } from "../controller/postController.js";
 const router= Router();
 
 router.post("/user",CreateUser)
@@ -8,7 +9,12 @@ router.post("/updateuaser/:id",updateuaser)
 router.delete("/deleteuser/:id",deleteuser)
 router.get("/getAllUsers/:email",getAllUsers)
 
+//postapiurl
 
+router.post("/createpost",createpost)
+
+
+router.get("/getAllposts/:user_id", getAllposts);
 
 
 
